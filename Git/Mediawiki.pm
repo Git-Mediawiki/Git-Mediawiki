5aaf2137b691fc8b6ba49d55097e2a131c871d9b
Mark A. Hershberger <mah@nichework.com>
2019-05-08 12:04:03 -0400

    Add Copyright and license headers

--- Git/Mediawiki.pm
+++ Git/Mediawiki.pm
@@ -1,5 +1,26 @@
 package Git::Mediawiki;
 
+# Copyright (C) 2013  Benoit Person  <benoit.person@ensimag.imag.fr>
+# Copyright (C) 2017  Antoine Beaupré
+# Copyright (C) 2017  Torbjörn Lönnemark
+# Copyright (C) 2018  Simon Legner
+# Copyright (C) 2019  Mark A. Hershberger
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 use 5.008;
 use strict;
 use POSIX;
